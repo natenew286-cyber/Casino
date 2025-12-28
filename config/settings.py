5391ebdcb55da11d2553fc42125651ab438d9315
thisishammo <hammondkakhayanga@gmail.com>
2025-12-28 15:57:20 +0300

    Stage SECRET_KEY to use .env variables

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)2et@9@@=^%n10oe+b%z5_#f%rk@n(a4&i*36i&!jtaqyybldk'
+# SECRET_KEY should be set in .env file. Fallback value is for development only.
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
